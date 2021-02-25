0c4d8f7155a057398d92a231032713b93e866e14
Lany75 <mlanie.parry@gmail.com>
2021-02-25 14:23:17 +0100

    mise à jour de l'app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import './App.css';
-import imgBoisRosoy from './images/logo-bois-de-rosoy.png';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+import './App.css';
+
+import imgBoisRosoy from './images/logo-bois-de-rosoy.png';
+import escapeForest from './images/Logo-ESCAPE-FOREST-noir-150x150.png';
+
+
+
 
 const App = () => {
-  let tailleOK = true;
-
-  const verifTailleNbr = (nombre) => {
-
-    if (nombre.length !== 2) {
-      tailleOK = false;
-      afficheMessage("vous devez entrer des nombres à 2 chiffres")
-
-    } else {
-      tailleOK = true;
-      afficheMessage("");
-    }
-
-  }
+  const numeros = ["0", "6", "1", "0", "9", "7", "6", "6", "1", "4"];
 
   const afficheMessage = (message) => {
     const divMessage = document.getElementById("message");
@@ -28,60 +20,81 @@ const App = () => {
 
   const verifChiffres = () => {
 
-    const chiffre1 = document.getElementById("chiffre-1").value;
-    const chiffre2 = document.getElementById("chiffre-2").value;
-    const chiffre3 = document.getElementById("chiffre-3").value;
-    const chiffre4 = document.getElementById("chiffre-4").value;
-    const chiffre5 = document.getElementById("chiffre-5").value;
+    const chiffres = [];
 
-    verifTailleNbr(chiffre1);
-    if (tailleOK === true) verifTailleNbr(chiffre2);
-    if (tailleOK === true) verifTailleNbr(chiffre3);
-    if (tailleOK === true) verifTailleNbr(chiffre4);
-    if (tailleOK === true) verifTailleNbr(chiffre5);
+    for (let i = 1; i <= 10; i++) {
+      chiffres.push(document.getElementById("chiffre-" + i).value);
+    }
+
+    const tailleOk = chiffres.every(currentValue => {
+      return currentValue.length === 1;
+    })
+
+    if (!tailleOk) afficheMessage("Vous devez entrer des chiffres ...")
+    else {
+      let goodNumber = true;
+
+      afficheMessage("");
 
 
-    if (tailleOK === true) {
-      if (chiffre1.toString() !== "06") {
-        afficheMessage("Votre 1er nombre n'est pas correct");
-      } else {
-        if (chiffre2.toString() !== "10") {
-          afficheMessage("Votre 2ème nombre n'est pas correct");
-        } else {
-          if (chiffre3.toString() !== "97") {
-            afficheMessage("Votre 3ème nombre n'est pas correct");
-          } else {
-            if (chiffre4.toString() !== "66") {
-              afficheMessage("Votre 4ème nombre n'est pas correct");
-            } else {
-              if (chiffre5.toString() !== "14") {
-                afficheMessage("Votre 5ème nombre n'est pas correct");
-              } else {
-                afficheMessage("Bravo !! Vous avez trouvez tous les nombre, vous pouvez appeler ce numéro");
-              }
-            }
-          }
+      for (let i = 0; i < chiffres.length; i++) {
+        if (chiffres[i] !== numeros[i]) {
+          goodNumber = false;
         }
       }
+
+      if (goodNumber) afficheMessage("Félicitations ! C'est le bon numéro. Contactez vite Axelle")
+      else afficheMessage("Oups ! Le numéro n'est pas correct. Besoin d'aide ? Contactez findthetreasure@gmail.com")
+
     }
   }
 
   return (
     <div className="App">
-      <div id="logo"><img src={imgBoisRosoy} alt="Le bois de Rosoy" /></div>
-      <div id="text-accueil">Entrez les nombres trouvés :</div>
-      <div id="inputs">
-        <div className="input"><TextField id="chiffre-1" label="1er nombre" variant="outlined" type="number" /></div>
-        <div className="input"><TextField id="chiffre-2" label="2ème nombre" variant="outlined" type="number" /></div>
-        <div className="input"><TextField id="chiffre-3" label="3ème nombre" variant="outlined" type="number" /></div>
-        <div className="input"><TextField id="chiffre-4" label="4ème nombre" variant="outlined" type="number" /></div>
-        <div className="input"><TextField id="chiffre-5" label="5ème nombre" variant="outlined" type="number" /></div>
+
+      <div id="logo">
+        <img id="logo-bois-rosoy" src={imgBoisRosoy} alt="Le bois de Rosoy" />
+        <img id="logo-escape-forest" src={escapeForest} alt="Escape Forest" />
       </div>
+
+      <h1>Allez vous trouver le trésor ?</h1>
+
+      <div id="numeros-trouves">
+
+        <div id="text-accueil">Entrez le numéro de téléphone</div>
+
+        <div id="inputs">
+          <div className="input-group">
+            <div className="input"><TextField id="chiffre-1" variant="outlined" type="number" /></div>
+            <div className="input"><TextField id="chiffre-2" variant="outlined" type="number" /></div>
+          </div>
+          <div className="input-group">
+            <div className="input"><TextField id="chiffre-3" variant="outlined" type="number" /></div>
+            <div className="input"><TextField id="chiffre-4" variant="outlined" type="number" /></div>
+          </div>
+          <div className="input-group">
+            <div className="input"><TextField id="chiffre-5" variant="outlined" type="number" /></div>
+            <div className="input"><TextField id="chiffre-6" variant="outlined" type="number" /></div>
+          </div>
+          <div className="input-group">
+            <div className="input"><TextField id="chiffre-7" variant="outlined" type="number" /></div>
+            <div className="input"><TextField id="chiffre-8" variant="outlined" type="number" /></div>
+          </div>
+          <div className="input-group">
+            <div className="input"><TextField id="chiffre-9" variant="outlined" type="number" /></div>
+            <div className="input"><TextField id="chiffre-10" variant="outlined" type="number" /></div>
+          </div>
+        </div>
+
+        <div id="bouton-verif">
+          <Button variant="contained" onClick={verifChiffres}>Verifiez !!</Button>
+        </div>
+
+      </div>
+
       <div id="message"></div>
-      <div id="bouton-verif">
-        <Button variant="contained" onClick={verifChiffres}>Verifiez !!</Button>
-      </div>
-    </div>
+
+    </div >
   );
 }
 
